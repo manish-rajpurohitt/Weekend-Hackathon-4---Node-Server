ea8719d69a5b823f80a6bb09974a7dafb8e241d2
Manish Purohit <manishkumar9866@gmail.com>
2020-11-12 15:08:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  if(req.url === "/welcome"){
+      res.statusCode = 200;
+    res.end("Welcome to Dominos!");
+  }
+  else if(req.url === "/contact"){
+      res.statusCode = 200;
+    res.end(JSON.stringify({
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+        }));
+  }
+  else{
+      res.statusCode = 404;
+      res.end();
+  }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
