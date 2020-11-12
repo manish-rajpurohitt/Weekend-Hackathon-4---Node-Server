bd8603c7d9b1b4915927a9504e357def7cd3efa5
Manish Purohit <manishkumar9866@gmail.com>
2020-11-12 15:38:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require("http");
 const httpServer = http.createServer(handleServer);
 const lol = `{phone: '18602100000',email: 'guestcaredominos@jublfood.com'}`;
 
-function handleServer(req, res) {
+function handleServer(rq, rs) {
     if(rq.url ==="/welcome")
     {   
         rs.writeHead(200, { 'Content-Type': 'text/plain' });
